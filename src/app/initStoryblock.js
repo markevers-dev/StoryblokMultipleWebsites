86f12c6573a84ef98cda58d72e1c714099b7f69e
Mark <m.evers@lukkien.com>
2024-12-09 08:10:59 +0100

    replaced the token with env
    The space that token was for doesn't exist anymore so I didn't care about putting it directly in code, but this is a better example

--- src/app/initStoryblock.js
+++ src/app/initStoryblock.js
@@ -2,7 +2,7 @@ import { storyblokInit, apiPlugin } from "@storyblok/react";
 
 export default function initStoryblok(options = {}) {
   storyblokInit({
-    accessToken: "a9j2QNlmQXdbF6JuO4p5Vgtt",
+    accessToken: process.env.STORYBLOK_ACCESS_TOKEN,
     use: [apiPlugin],
     ...options,
   });
